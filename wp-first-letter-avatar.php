1502da96e09388e76bf11473c06c3d1fdc9a429f
Dev49.net <info@dev49.net>
2016-03-07 02:20:07 +0100

    moved avatar file name detection logic inside the conditional statement

--- wp-first-letter-avatar.php
+++ wp-first-letter-avatar.php
@@ -382,85 +382,88 @@ class WP_First_Letter_Avatar {
 	private function generate_first_letter_uri($name, $size){
 
 		// get picture filename (and lowercase it) from commenter name:
-		if (empty($name)){  // if, for some reason, the result is empty, set file_name to default unknown image
+		if (empty($name)){  // if, for some reason, the name is empty, set file_name to default unknown image
+
 			$file_name = $this->image_unknown;
-			$file_name_mb = $this->image_unknown;
-		} else {
+
+		} else { // name is not empty, so we can proceed
+
 			$file_name = substr($name, $this->letter_index, 1); // get one letter counting from letter_index
 			$file_name = strtolower($file_name); // lowercase it...
 			$file_name_mb = mb_substr($name, $this->letter_index, 1); // repeat, this time with multibyte functions
 			$file_name_mb = mb_strtolower($file_name_mb); // and again...
-		}
 
-		// couple of exceptions:
-		if ($file_name_mb == 'ą'){
-			$file_name = 'a';
-			$file_name_mb = 'a';
-		} else if ($file_name_mb == 'ć'){
-			$file_name = 'c';
-			$file_name_mb = 'c';
-		} else if ($file_name_mb == 'ę'){
-			$file_name = 'e';
-			$file_name_mb = 'e';
-		} else if ($file_name_mb == 'ń'){
-			$file_name = 'n';
-			$file_name_mb = 'n';
-		} else if ($file_name_mb == 'ó'){
-			$file_name = 'o';
-			$file_name_mb = 'o';
-		} else if ($file_name_mb == 'ś'){
-			$file_name = 's';
-			$file_name_mb = 's';
-		} else if ($file_name_mb == 'ż' || $file_name_mb == 'ź'){
-			$file_name = 'z';
-			$file_name_mb = 'z';
-		}
-
-		// create arrays with allowed character ranges:
-		$allowed_numbers = range(0, 9);
-		foreach ($allowed_numbers as $number){ // cast each item to string (strict param of in_array requires same type)
-			$allowed_numbers[$number] = (string)$number;
-		}
-		$allowed_letters_latin = range('a', 'z');
-		$allowed_letters_cyrillic = range('а', 'ё');
-		$allowed_letters_arabic = range('آ', 'ی');
-		// check if the file name meets the requirement; if it doesn't - set it to unknown
-		$charset_flag = ''; // this will be used to determine whether we are using latin chars, cyrillic chars, arabic chars or numbers
-		// check whther we are using latin/cyrillic/numbers and set the flag, so we can later act appropriately:
-		if (in_array($file_name, $allowed_numbers, true)){
-			$charset_flag = 'number';
-		} else if (in_array($file_name, $allowed_letters_latin, true)){
-			$charset_flag = 'latin';
-		} else if (in_array($file_name, $allowed_letters_cyrillic, true)){
-			$charset_flag = 'cyrillic';
-		} else if (in_array($file_name, $allowed_letters_arabic, true)){
-			$charset_flag = 'arabic';
-		} else { // for some reason none of the charsets is appropriate
-			$file_name = $this->image_unknown; // set it to uknknown
-		}
-
-		if (!empty($charset_flag)){ // if charset_flag is not empty, i.e. flag has been set to latin, number or cyrillic...
-			switch ($charset_flag){ // run through various options to determine the actual filename for the letter avatar
-				case 'number':
-					$file_name = 'number_' . $file_name;
-					break;
-				case 'latin':
-					$file_name = 'latin_' . $file_name;
-					break;
-				case 'cyrillic':
-					$temp_array = unpack('V', iconv('UTF-8', 'UCS-4LE', $file_name_mb)); // beautiful one-liner by @bobince from SO - http://stackoverflow.com/a/27444149/4848918
-					$unicode_code_point = $temp_array[1];
-					$file_name = 'cyrillic_' . $unicode_code_point;
-					break;
-				case 'arabic':
-					$temp_array = unpack('V', iconv('UTF-8', 'UCS-4LE', $file_name_mb));
-					$unicode_code_point = $temp_array[1];
-					$file_name = 'arabic_' . $unicode_code_point;
-					break;
-				default:
-					$file_name = $this->image_unknown; // set it to uknknown
-					break;
+			// couple of exceptions:
+			if ($file_name_mb == 'ą'){
+				$file_name = 'a';
+				$file_name_mb = 'a';
+			} else if ($file_name_mb == 'ć'){
+				$file_name = 'c';
+				$file_name_mb = 'c';
+			} else if ($file_name_mb == 'ę'){
+				$file_name = 'e';
+				$file_name_mb = 'e';
+			} else if ($file_name_mb == 'ń'){
+				$file_name = 'n';
+				$file_name_mb = 'n';
+			} else if ($file_name_mb == 'ó'){
+				$file_name = 'o';
+				$file_name_mb = 'o';
+			} else if ($file_name_mb == 'ś'){
+				$file_name = 's';
+				$file_name_mb = 's';
+			} else if ($file_name_mb == 'ż' || $file_name_mb == 'ź'){
+				$file_name = 'z';
+				$file_name_mb = 'z';
 			}
+
+			// create arrays with allowed character ranges:
+			$allowed_numbers = range(0, 9);
+			foreach ($allowed_numbers as $number){ // cast each item to string (strict param of in_array requires same type)
+				$allowed_numbers[$number] = (string)$number;
+			}
+			$allowed_letters_latin = range('a', 'z');
+			$allowed_letters_cyrillic = range('а', 'ё');
+			$allowed_letters_arabic = range('آ', 'ی');
+			// check if the file name meets the requirement; if it doesn't - set it to unknown
+			$charset_flag = ''; // this will be used to determine whether we are using latin chars, cyrillic chars, arabic chars or numbers
+			// check whther we are using latin/cyrillic/numbers and set the flag, so we can later act appropriately:
+			if (in_array($file_name, $allowed_numbers, true)){
+				$charset_flag = 'number';
+			} else if (in_array($file_name, $allowed_letters_latin, true)){
+				$charset_flag = 'latin';
+			} else if (in_array($file_name, $allowed_letters_cyrillic, true)){
+				$charset_flag = 'cyrillic';
+			} else if (in_array($file_name, $allowed_letters_arabic, true)){
+				$charset_flag = 'arabic';
+			} else { // for some reason none of the charsets is appropriate
+				$file_name = $this->image_unknown; // set it to uknknown
+			}
+
+			if (!empty($charset_flag)){ // if charset_flag is not empty, i.e. flag has been set to latin, number or cyrillic...
+				switch ($charset_flag){ // run through various options to determine the actual filename for the letter avatar
+					case 'number':
+						$file_name = 'number_' . $file_name;
+						break;
+					case 'latin':
+						$file_name = 'latin_' . $file_name;
+						break;
+					case 'cyrillic':
+						$temp_array = unpack('V', iconv('UTF-8', 'UCS-4LE', $file_name_mb)); // beautiful one-liner by @bobince from SO - http://stackoverflow.com/a/27444149/4848918
+						$unicode_code_point = $temp_array[1];
+						$file_name = 'cyrillic_' . $unicode_code_point;
+						break;
+					case 'arabic':
+						$temp_array = unpack('V', iconv('UTF-8', 'UCS-4LE', $file_name_mb));
+						$unicode_code_point = $temp_array[1];
+						$file_name = 'arabic_' . $unicode_code_point;
+						break;
+					default:
+						$file_name = $this->image_unknown; // set it to uknknown
+						break;
+				}
+			}
+
 		}
 
 		// detect most appropriate size based on WP avatar size:
